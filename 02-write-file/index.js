a1724537bb7f1457c569a374a5e6bdedda5af6e5
minimonster22 <tamaramonstr@gmail.com>
2024-01-22 01:40:06 +0300

    feat: add problem solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const filePath = '02-write-file/output.txt';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+console.log('Welcome! Enter text (type "exit" to terminate):');
+
+rl.on('line', (input) => {
+    if (input.toLowerCase() === 'exit') {
+        console.log('Farewell!');
+        rl.close();
+    } else {
+        writeStream.write(input + '\n');
+    }
+});
+
+rl.on('close', () => {
+    process.exit();
+});
